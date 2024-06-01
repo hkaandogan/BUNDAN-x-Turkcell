9c2e5008355f486a899150562ee5056f10ee2637
Kaan Doğan <doganh19@itu.edu.tr>
2024-06-01 17:52:49 +0300

    build try 1

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   transpileDependencies: [
-    'vuetify'
+    'vuetify',
+    '@google/model-viewer'
   ],
-  pages:{
+  pages: {
     index: {
       entry: "src/main.js",
       template: "public/index.html",
       filename: "index.html",
       title: "Bundan App",
     },
+  },
+  chainWebpack: (config) => {
+    // Add a rule to include transpilation of @google/model-viewer
+    config.module
+      .rule('js')
+      .include
+      .add(/node_modules\/@google\/model-viewer/)
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader');
   }
 }
